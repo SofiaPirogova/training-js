abf663ae5dc33dae53d03b497297fa629a9b39f5
Sofia <73636331+SofiaPirogova@users.noreply.github.com>
2022-01-17 23:24:44 +0300

    modul-3.2

--- js-3/modul-3.2.js
+++ js-3/modul-3.2.js
@@ -83,7 +83,7 @@ const Transaction = {
   // положить
   DEPOSIT: 'deposit',
   // снять
-  WITHGRAW: 'withdraw',
+  WITHDRAW: 'withdraw',
 };
 
 const account = {
@@ -109,12 +109,39 @@ const account = {
       amount,
     };
   },
+  // метод чтобы не могли ввести ничего кроме чисел в снятие и добавление
+  validateAmount(amount) {
+    // проверяем на тру или фолс
+    // 1
+    // if (typeof amount !== 'number') {
+    //   console.log('Ошибка!Введите цифры!');
+    //   return false;
+    // } else {
+    //   return true;
+    // }
+    // 2
+    return typeof amount === 'number';
+  },
 
   // метод отвечающий за добавление суммы к балансу
   // принимает сумму транзакции
   // вызывает createTransaction() для создания объекта транзакции
   // после чего добавляет его в историю транзакции
   deposit(amount) {
+    // иф чтобы не могли ввести ничего кроме чисел - перенесли в отдельную функцию
+    // 1
+    // if (typeof amount !== 'number') {
+    //   console.log('Ошибка!Введите цифры!');
+    //   return;
+    // }
+    // 2
+    // const isValid = this.validateAmount(amount);
+    // console.log('isValid = ', isValid);
+    // if (!isValid) return;
+    // если фолс, то не допускает эту транзакцию
+    // 3 запись покороче
+    if (!this.validateAmount(amount)) return;
+
     this.balance += amount;
     const transac = this.createTransaction(amount, Transaction.DEPOSIT);
     this.transactions.push(transac);
@@ -126,16 +153,52 @@ const account = {
   // после чего доавляет его в историю транзакций
   // если amount больше чем текуший баланс, вывести сообщение
   // о том, что снятие такой суммы не возможно, недостаточно средств
-  withdraw(amount) {},
+  withdraw(amount) {
+    // 1
+    // if (typeof amount !== 'number') {
+    //   console.log('Ошибка!Введите цифры!');
+    //   return;
+    // }
+    // 2
+    const isValid = this.validateAmount(amount);
+    // console.log('isValid = ', isValid);
+    if (!isValid) return;
+
+    if (amount > this.balance) {
+      return console.log('Недостаточно средств для выполнения операции');
+    }
+    this.balance -= amount;
+    const transac = this.createTransaction(amount, Transaction.WITHDRAW);
+    this.transactions.push(transac);
+  },
 
   // метод возвращает текущий баланс
-  getBalance() {},
+  getBalance() {
+    return this.balance;
+  },
   // метод ищет и возвращает объект транзакции по id
-  getTransactionDetails(is) {},
+  getTransactionDetails(id) {
+    for (const transac of this.transactions) {
+      // ищем транзакцию с нужным нам айди
+      if (transac.id === id) {
+        return transac;
+      }
+    }
+    // если ничего не нашел
+    return null;
+  },
 
   // Метод возвращает количество средств
   // определенного типа транзакции из всей истории транзакций
-  getTransactionTotal(type) {},
+  getTransactionTotal(type) {
+    let total = 0;
+    for (const transac of this.transactions) {
+      if (transac.type === type) {
+        total += transac.amount;
+      }
+    }
+    return total;
+  },
 };
 
 // console.log(account.createTransaction(100, Transaction.DEPOSIT));
@@ -143,6 +206,25 @@ const account = {
 console.log(JSON.stringify(account, null, 2)); //наглядно сверить
 // положим 100р
 account.deposit(100);
+// пытаемся сбить с толку вводя строку
+account.deposit('100');
+account.deposit(80);
 // смотрим как появилась транзакция в массиве транзакций
-console.log(JSON.stringify(account, null, 2)); // null,2 чтобы симпатично печаталось
+// console.log(JSON.stringify(account, null, 2)); // null,2 чтобы симпатично печаталось
 console.log(account);
+// снимем 50 р транзакция прошла
+account.withdraw(50);
+account.withdraw('30');
+account.withdraw(20);
+console.log(account);
+// снимем 250р - транзакция не проходит, так как денег больше снимается,
+// чем есть в балансе
+account.withdraw(250);
+console.log(account);
+// смотрим баланс отдельно
+console.log(account.getBalance());
+// нашли конакретную транзацию по айди
+console.log(account.getTransactionDetails(2));
+// смотрим сумму по типу транзакции
+console.log(account.getTransactionTotal(Transaction.DEPOSIT));
+console.log(account.getTransactionTotal(Transaction.WITHDRAW));
